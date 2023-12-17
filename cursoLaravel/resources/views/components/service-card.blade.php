56d839e0b9c177f1c4cf937768851923afd27721
DevJJMendez <DevJJMendez@gmail.com>
2023-12-17 10:55:06 -0500

    feat

--- cursoLaravel/resources/views/components/service-card.blade.php
+++ cursoLaravel/resources/views/components/service-card.blade.php
@@ -1,5 +1,5 @@
 <div class="card" style="width: 18rem;">
-    <img src="{{ $imageLink }}" class="card-img-top" alt="...">
+    <img src="{{asset('assets/image/laravel-logo.png')}}" class="card-img-top" alt="laravel-logo">
     <div class="card-body">
       <h5 class="card-title">{{ $title }}</h5>
       <p class="card-text">{{ $content }}</p>
--- cursoLaravel/resources/views/layouts/_partials/navbar.blade.php
+++ cursoLaravel/resources/views/layouts/_partials/navbar.blade.php
@@ -1,22 +1,22 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-danger">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Laravel</a>
+      <a class="navbar-brand text-light" href="#">Laravel</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav text-light">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{ route('index') }}">Home</a>
+            <a class="nav-link active text-light" aria-current="page" href="{{ route('index') }}">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('about') }}">About</a>
+            <a class="nav-link text-light" href="{{ route('about') }}">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('services') }}">Services</a>
+            <a class="nav-link text-light" href="{{ route('services') }}">Services</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('contact') }}">Contact</a>
+            <a class="nav-link text-light" href="{{ route('contact') }}">Contact</a>
           </li>
           {{-- <li class="nav-item">
             <a class="nav-link disabled" aria-disabled="true">Disabled</a>
